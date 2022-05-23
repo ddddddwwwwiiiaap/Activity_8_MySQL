f158b227241bd1fa1355c56eb80b73bf83b581f8
dwiaprilyaanggoroputry <dwi.aprilya.ft20@mail.umy.ac.id>
2022-05-23 15:53:20 +0700

    Activity 10 Upload file php dan database kedalam web server

--- app/src/main/java/com/example/activity_8_mysql/EditTeman.java
+++ app/src/main/java/com/example/activity_8_mysql/EditTeman.java
@@ -31,7 +31,7 @@ public class EditTeman extends AppCompatActivity {
     String id, nm, tlp, namaEd, telponEd;
     int sukses;
 
-    private  static  String url_update = "http://192.168.100.9/umyTI/updatetm.php";
+    private  static  String url_update = "https://20200140055.praktikumtiumy.com/updatetm.php";
     private  static  final  String TAG = EditTeman.class.getSimpleName();
     private static final String TAG_SUCCES = "success";
 
